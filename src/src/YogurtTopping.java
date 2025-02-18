b7a2b125a1b439430651a7b5877a611bf5aeee2d
Adib Rahman <adibrahman44@gmail.com>
2025-02-18 11:29:11 +0600

    Created YogurtTopping Class

---
+++ src/src/YogurtTopping.java
@@ -0,0 +1,12 @@
+public class YogurtTopping {
+    private String name;
+    private double price;
+
+    public YogurtTopping(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+}
